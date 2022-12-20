c5df224bfd0a0a09169edcbb4e2193a02a538679
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-12-20 19:15:22 +0100

    Initial Commit

---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Piotr Woźniak on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
